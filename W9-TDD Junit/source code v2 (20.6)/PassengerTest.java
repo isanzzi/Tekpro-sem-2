c5e918c07d1e2651a55ed7c12c9b7ad2f1a2f66c
SiKodot <ihsangamersfauzi123@gmail.com>
2025-05-02 23:25:35 +0700

    W9-20.6

---
+++ W9-TDD Junit/source code v2 (20.6)/PassengerTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PassengerTest {
+    Passenger ajay;
+    @BeforeEach
+    void setUp() {
+        ajay = new Passenger("Azzar", true);
+    }
+
+    @Test
+    void getName() {
+        assertEquals("Azzar", ajay.getName());
+    }
+
+    @Test
+    void isVip() {
+        assertEquals(false, ajay.isVip());
+    }
+}
