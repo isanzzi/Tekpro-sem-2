e89d3a07fd0dde29133d0bfa3f856e3783b63982
SiKodot <ihsangamersfauzi123@gmail.com>
2025-05-17 22:57:18 +0700

    W9-20.8

---
+++ W9-TDD Junit/source code v2 (20.8)/FlightTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getPassengersList() {
+    }
+
+    @Test
+    void getFlightType() {
+    }
+
+    @Test
+    void addPassenger() {
+    }
+
+    @Test
+    void removePassenger() {
+    }
+}
