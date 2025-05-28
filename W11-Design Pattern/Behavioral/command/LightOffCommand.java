5def9b84d20e7f7350b72bfcb99ab106fb980614
SiKodot <ihsangamersfauzi123@gmail.com>
2025-05-28 23:30:17 +0700

    command

---
+++ W11-Design Pattern/Behavioral/command/LightOffCommand.java
@@ -0,0 +1,11 @@
+public class LightOffCommand implements Command{
+    private  Light light;
+
+    LightOffCommand (Light light){
+        this.light = light;
+    }
+
+    public  void execute (){
+        light.off();
+    }
+}
